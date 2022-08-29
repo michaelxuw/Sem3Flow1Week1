43387c3a9304d1d72d823780b8a372f1a15bf3bf
michaelxuw <michaelxuwrelated@gmail.com>
2022-08-29 13:57:29 +0200

    this should be it?

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MyTest {
 
